6ec844b589b525e5dc3edb2e8b7702010edc6611
saikat samanta <saikatsamanta052@gmail.com>
2023-09-08 02:50:42 +0530

    herosection

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -3,7 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Home() {
   return (
     <>
-    div
+    <div className='h-96 bg-[#FBEDC9] w-full rounded-b-[50px]'>
+        <div className="pattern h-48 w-72 absolute top-30 rotate-12 -left-14 rounded-sm bg-[#FFF6DE]"></div>
+        <div className="pattern h-48 w-72 absolute top-40 -rotate-12 left-[60%] rounded-sm bg-[#FFE7A9]"></div>
+        <div className="pattern h-48 w-72 absolute top-44 -rotate-12 -left-[50%] rounded-sm bg-[#f7f7f7]"></div>
+        <div className="pattern h-48 w-72 absolute top-30 rotate-12 -left-14 rounded-sm bg-[#FFF6DE]"></div>
+    </div>
     </>
   )
 }
