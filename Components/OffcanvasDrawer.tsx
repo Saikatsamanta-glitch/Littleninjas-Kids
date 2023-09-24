daac4cdf41841d6c8470a607f43e2cf5ad285426
Debnath-Sinhababu <debnathsinhababu2017@gmail.com>
2023-09-24 13:37:30 +0530

    registration form

--- Components/OffcanvasDrawer.tsx
+++ Components/OffcanvasDrawer.tsx
@@ -9,56 +9,81 @@ import { Course_context } from '@/context/course_context';
 import OtpInput from 'react-otp-input';
 import auth from "../firebaseconfig";
 import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
-
+import { ToastContainer, toast } from 'react-toastify';
 
 const OffcanvasDrawer = ({ show, setShow }: any) => {
-        var recaptchaVerifier="";
-        const handleClose = () => { setShow(false) }
+       let recaptchaVerifier=null
+        const handleClose = () => { 
+            setShowChecker(true)
+            setOtp('')
+            setphone('')
+            setShow(false) }
         const [phone, setphone] = useState<string>('')
         const [otp, setOtp] = useState<string>('');
         const [showchecker, setShowChecker] = useState<boolean>(true);
         const [disable, setDisable] = useState<boolean>(true);
+        const [confirmResult,setconfirm]=useState()
         const data = useContext(Course_context)
-        console.log(data)
+        console.log(showchecker)
 
         const checkBtn = (e) => {
                 e.preventDefault();
                 console.log(phone);
-                recaptchaVerifier = new RecaptchaVerifier(
+              
+                      recaptchaVerifier = new RecaptchaVerifier(
                         auth,
-                        "recaptchaverifier",
-                        {
-                                size: "normal",
-                                callback: (response: any) => {
-                                        console.log(response);
-                                        setTimeout(() => {
-                                                setShowChecker(false)
-                                        }, 500);
-
-                                        // reCAPTCHA solved, allow signInWithPhoneNumber.
+                                "recaptcha",
+                                {
+                                        size: "normal",
+                                        callback: (response: any) => {
+                                                console.log(response);
+                                                getOtp()
+                                                setTimeout(() => {
+                                                        setShowChecker(false)
+                                                }, 500);
+        
+                                                // reCAPTCHA solved, allow signInWithPhoneNumber.
+                                        },
                                 },
-                        });
-                
+                                
+                                
+                                );
+                           
+               recaptchaVerifier.render()
 
         };
-        const confirmOtp = () => {
+        const getOtp = () => {
                 signInWithPhoneNumber(auth, "+" + phone, recaptchaVerifier)
                         .then((confirmationResult) => {
-                                confirmationResult.confirm(otp).then((result) => {
-                                        // User signed in successfully.
-                                        setDisable(false);
-                                        alert("success")
-                                        // ...
-                                }).catch((error) => {
-                                        // User couldn't sign in (bad verification code?)
-                                        // ...
-                                        alert("failed")
-                                });
+                                console.log(confirmationResult,'confirm')
+                                toast.success('otp sent successfully')
+                              setconfirm(confirmationResult)
+                                
                         })
-                        .catch(() => {
-
+                        .catch((err) => {
+                                toast.error(err.message)
                         })
         }
+       const confirmOtp=(e)=>{
+        e.preventDefault()
+        confirmResult?.confirm(otp).then((result) => {
+            // User signed in successfully.
+            console.log(result)
+            setDisable(false);
+            setShowChecker(true)
+            toast.success('otp verified successfully')
+            // ...
+    }).catch((error) => {
+            // User couldn't sign in (bad verification code?)
+            // ...
+            toast.error('oops! Something went wrong')
+    });
+       }
+    const submitRegistration=(e)=>{
+        e.preventDefault();
+        alert('submit')
+    }
+
 
         return (
                 <Offcanvas show={show} onHide={handleClose} placement='end' style={{ width: '500px' }}>
@@ -69,10 +94,10 @@ const OffcanvasDrawer = ({ show, setShow }: any) => {
                         </Offcanvas.Header>
                         <Offcanvas.Body>
                                 <p>Please Fill all the entries</p>
-                                <div>
+                                <Form onSubmit={submitRegistration}>
                                         <div className="mb-3">
                                                 <Form.Label>Email address</Form.Label>
-                                                <Form.Control type="email" placeholder="Enter email" />
+                                                <Form.Control type="email" placeholder="Enter email" required />
                                                 <Form.Text className="text-muted">
                                                         We'll never share your email with anyone else.
                                                 </Form.Text>
@@ -80,10 +105,10 @@ const OffcanvasDrawer = ({ show, setShow }: any) => {
 
                                         <div className="mb-3" >
                                                 <Form.Label>Name</Form.Label>
-                                                <Form.Control type="text" placeholder="Name" />
+                                                <Form.Control type="text" placeholder="Name" required/>
                                         </div>
                                         <div className="mb-3">
-                                                <Form.Select aria-label="Default select example">
+                                                <Form.Select aria-label="Default select example" required>
                                                         <option>Select Course</option>
                                                         {
                                                                 data.map((obj, i) => {
@@ -94,6 +119,7 @@ const OffcanvasDrawer = ({ show, setShow }: any) => {
                                                         }
 
                                                 </Form.Select>
+
 
                                         </div>
                                         <div className="mb-3 flex">
@@ -109,18 +135,24 @@ const OffcanvasDrawer = ({ show, setShow }: any) => {
                                                 </button>
 
                                         </div>
-                                        <div  id="recaptchaverifier"></div>
-                                        {<> <OtpInput
+                                        {
+                                         showchecker &&   <div  id="recaptcha"></div>
+                                        }
+                                      
+                                        {
+                                   !showchecker &&         <div className='flex w-full mb-3 md:flex-row flex-col'> <OtpInput
                                                 value={otp}
                                                 onChange={setOtp}
                                                 numInputs={6}
-                                                renderSeparator={<span>-</span>}
+                                                renderSeparator={<pre>  </pre>}
                                                 renderInput={(props) => <input {...props} /> }
+                                                containerStyle={{}}
+                                                inputStyle={{border:'1px solid rgb(228,228,231)',width:'30px',outline:'none',opacity:1}}
                                         />
-                                                <button onClick={confirmOtp}>otp </button> 
-                                         </>}
+                                                <button onClick={confirmOtp} className=' bg-orange-500 border-none w-[100px] p-1 ml-4 text-white'>verify otp </button> 
+                                         </div>}
                                         <div className="mb-3">
-                                                <Form.Select aria-label="Default select example">
+                                                <Form.Select aria-label="Default select example" required>
                                                         <option>Select Grade</option>
                                                         <option value='1'>Grade 1-5</option>
                                                         <option value='2'>Grade 6-10</option>
@@ -130,10 +162,10 @@ const OffcanvasDrawer = ({ show, setShow }: any) => {
                                         </div>
 
 
-                                        <Button disabled={disable} variant="primary" style={{ backgroundColor: 'orange', border: 'none' }}>
+                                        <Button disabled={disable} type='submit' variant="primary" style={{ backgroundColor: 'orange', border: 'none' }}>
                                                 Submit
                                         </Button>
-                                </div>
+                                </Form>
                         </Offcanvas.Body>
                 </Offcanvas>
         )
