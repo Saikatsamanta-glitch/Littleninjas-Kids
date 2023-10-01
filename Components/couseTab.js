f58a640d09fa11b43bdff04a0969ac33e6a697b7
saikat samanta <saikatsamanta052@gmail.com>
2023-10-02 00:31:04 +0530

    courses final

--- Components/couseTab.js
+++ Components/couseTab.js
@@ -16,7 +16,7 @@ export default function CourseTab({ data = {} }) {
                 })()
 
         }, [data.level_id])
-        // console.log(sessions );
+        console.log(data.character_logo);
         return (
 
                 <div className="h-[450px] w-80 hover:scale-105 transition-all duration-200  p-2 bg-[#FFF6DE] rounded-lg relative md:scale-95 scale-90 flex-shrink-0 flex flex-col justify-between ">
@@ -26,7 +26,7 @@ export default function CourseTab({ data = {} }) {
                                 backgroundColor: color,
                         }} className={`flex justify-evenly items-center w-full h-28 md:mt-4 mt-2 rounded-lg `}>
 
-                                <img src="/level4.png" alt="" className="w-[30%] scale-[1.6]" />
+                                <img src={data.character_logo} alt="" className="w-[30%] scale-[1.6]" />
 
                                 <div className="pr-2 h-full w-auto flex flex-col  justify-center text-center ">
                                         <h1 className="text-center text-xl text-black/75 font-bold">Become <br /> {data[" title"]} </h1>
@@ -55,7 +55,7 @@ export default function CourseTab({ data = {} }) {
                                 <Button size="sm" onClick={() => setShow(!show)} className='bg-[#FFDF8C] text-sm font-normal border-2  text-[#545454] hover:text-white hover:bg-[#FF847E]'>  Get Demo!</Button>
 
                         </div>
-                        <img src="/level1blade.png" alt="" className="h-20 absolute -top-6 right-0" />
+                        <img src={data.weapon_logo} alt="" className="h-20 absolute -top-6 right-0" />
                         <OffcanvasDrawer show={show} setShow={setShow} />
                 </div>
         )
